ad18bad71dc64ae4b02d129553b894c41913cc68
asmaiqbal85 <asmaiqbal85@gmail.com>
2024-10-06 04:55:53 -0700

    updated

---
+++ src/components/Hero.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div id='hero' className='min-h-screen bg-no-repeat bg-[url(/Asma Iqbal.jpg)] bg-left lg:bg-[15%] bg-cover'
+    style={{background: "35%"}}
+    >
+
+    </div>
+  )
+}
+
+export default Hero
